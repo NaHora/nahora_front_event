3e4f968529a7ad0836f706b968d8d5a884114618
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2023-01-24 20:42:04 -0300

    initial login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,12 +1,21 @@
-import { Button } from '@mui/material';
-import { Container } from './styles';
-import { useNavigate } from 'react-router-dom';
+import { Button } from "@mui/material";
+import { Container, EventImage } from "./styles";
+import { useNavigate } from "react-router-dom";
+import EventLogo from "../../assets/event-logo.png";
+import { Box } from "@mui/system";
 
 export const SignIn = () => {
   const navigate = useNavigate();
   return (
     <Container>
-      <Button onClick={() => navigate('/signup')}>SignIn</Button>
+      <EventImage src={EventLogo} alt="event logo" />
+      <Box
+        sx={{
+          width: "724px",
+          height: "660px",
+          backgroundColor: "#29282E",
+        }}
+      ></Box>
     </Container>
   );
 };
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,8 +1,13 @@
-import styled from 'styled-components';
-import { theme } from '../../styles/global';
+import styled from "styled-components";
+import { theme } from "../../styles/global";
 
 export const Container = styled.div`
-  background-color: ${theme.palette.primary.main};
-  width: 60px;
-  height: 60px;
+  background-color: ${theme.palette.secondary.main};
+  min-height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
+
+export const EventImage = styled.img``;
